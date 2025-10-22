682b60dfda08c046319baf2dfef7c4a5f7993999
Alpha <georgeofori2005@gmail.com>
2025-10-22 13:05:49 +0000

    Oct 22, 2025

--- scripts/business.js
+++ scripts/business.js
@@ -1,3 +1,7 @@
+//TEMPLATE_ID: template_dfce1fu
+//PUBLIC_KEY: hF3kg9QK6AUUcURu5
+//SERVICE_ID: service_ojfy4s4
+
 $(document).ready(function () {
 		let errorMessage, infoLabel;
 		$("label[for='username']").show().css("display", "inline-block");
@@ -24,75 +28,50 @@ $(document).ready(function () {
 		$("input[name='username']").on(
 		"keydown", function (event) {
 				if (event.key === 'Enter' && $(this).val().trim().length < 25 && $(this).val().trim().length > 0){
-											$("label[for='telephone']").removeClass('hidden').css('display', 'inline-block');
-						$("input[name='telephone']").removeClass('hidden').css('display', 'inline-block');
-						$("input[name='telephone']").focus();
+											$("label[for='email']").removeClass('hidden').css('display', 'inline-block');
+						$("input[name='email']").removeClass('hidden').css('display', 'inline-block');
+						$("input[name='email']").focus();
 				}
 		} 
 		);
 		
-		$("input[name='telephone']").on(
-		"input", function (event) {
-				let pattern = /^[\d\s#+*-]{10,20}$/;
-				let input = $(this).val().trim();
-				if (!pattern.test(input)){
-						if (!errorMessage){
-								errorMessage = $("<div>Your phone number should contain atleast 10-20 numbers and any of these: ()-+#* or space </div>");
-								errorMessage.css("color", "#A50044").insertAfter($(this));
-						}
-						errorMessage.show();
-						$(this).css('border-bottom', "3px solid #A50044");
-						setTimeout(() => {errorMessage.hide(); $(this).css('border-bottom', '1px solid rgba(0, 64, 156, 0.5)');}, 2000);
-				}else{
-						if(errorMessage){
-								errorMessage.hide();
-								$(this).css("border-bottom", "1px solid rgba(0, 64, 156, 0.5)");
-						}
-				}
-		}
-		);
 		
-		$("input[name='telephone']").on(
+		$("input[name='email']").on(
 		"keydown", function (event) {
-				let input = $(this).val().trim();
-				let pattern = /^[\d\s#+*-]{10,20}$/;
-				if (event.key === 'Enter') {
-						if (pattern.test(input)){
+			if (event.key === "Enter"){
 								$("label[for='message']").css("display","inline-block");
 								$("textarea[name='message']").css("display", "inline-block");
 								$("textarea[name='message']").focus();
-						}else{
-								if (!errorMessage){
-										errorMessage = $("<div> Your phone number should contain atleast 10-20 numbers and any of these: ()-+#* or space  </div>");
-										errorMessage.css("color", "#A50044").insertAfter($(this)).show();
-										$(this).css("border-bottom", "3px solid #A50044")
-										setTimeout(() => {errorMessage.hide();$(this).css("border-bottom", "1px solid rgba(0, 64, 156, 0.5)")}, 2000);
-								}else{
-										errorMessage.css("color", "#A50044").insertAfter($(this)).show();
-										$(this).css("border-bottom", "3px solid #A50044")
-										setTimeout(() => {errorMessage.hide();$(this).css("border-bottom", "1px solid rgba(0, 64, 156, 0.5)")}, 2000);
-								}
-						}
-				}
+			}
 		}
 		);
 		
 		$("textarea[name='message']").on(
 		"input", function (event) {
 			if (!$("input[name='sendButton']").is(":visible")){
-					$("input[type='button']").show(1500);
+					$("input[type='submit']").show(1500);
 			}
 				let input = $(this).val().trim();
 				if (!infoLabel){
 						infoLabel = $(`<div> ${input.length} / 300 </div>`);
-						infoLabel.css('color', 'green').insertAfter($(this)); 
+						infoLabel.insertAfter($(this)); 
 				}
 				infoLabel.html(`<div> ${input.length} / 300</div>`);
-if (input.length === 300)				{
-		infoLabel.css("color", "#A50044");
-}
+						if (input.length >= 300){
+								infoLabel.css({
+										"color": "#A50044",
+								});
+								$(this).css("border-bottom", "0.2rem solid #A50044");
+								setTimeout(() => {$(this).css("border-bottom", "0.2rem solid rgba(0, 64, 156, 0.5)"); infoLabel.css("color", "green");}, 2000);			
+						}else{
+								infoLabel.css({
+										"color": "green",
+								});
+						}
 				}
 		);
+		$(this).on("blur", function () {$(this).css("border-bottom", "0.2rem solid rgba(0, 64, 156, 0.5)");});
+		$(this).on('focus', function () {$(this).css("border-bottom", '0.2rem solid green');});
 		
 		
 }
