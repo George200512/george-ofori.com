582cacbc1e58261d94a20a5c065f527cbcb0d3ac
Alpha <georgeofori2005@gmail.com>
2025-10-24 14:50:31 +0000

    Oct 24, 2025

--- scripts/business.js
+++ scripts/business.js
@@ -1,9 +1,8 @@
-//TEMPLATE_ID: template_dfce1fu
-//PUBLIC_KEY: hF3kg9QK6AUUcURu5
-//SERVICE_ID: service_ojfy4s4
+import {TEMPLATE_ID, PUBLIC_KEY, SERVICE_ID} from "./secrets.js";
 
 $(document).ready(function () {
-		let errorMessage, infoLabel;
+		let errorMessage, infoLabel, counterLabel;
+		emailjs.init(PUBLIC_KEY);
 		$("label[for='username']").show().css("display", "inline-block");
 		$("input[name='username']").show().focus();
 		$("input[name='username']").on(
@@ -29,7 +28,7 @@ $(document).ready(function () {
 		"keydown", function (event) {
 				if (event.key === 'Enter' && $(this).val().trim().length < 25 && $(this).val().trim().length > 0){
 											$("label[for='email']").removeClass('hidden').css('display', 'inline-block');
-						$("input[name='email']").removeClass('hidden').css('display', 'inline-block');
+						$("input[name='email']").removeClass('hidden').css('display', 'block');
 						$("input[name='email']").focus();
 				}
 		} 
@@ -40,7 +39,7 @@ $(document).ready(function () {
 		"keydown", function (event) {
 			if (event.key === "Enter"){
 								$("label[for='message']").css("display","inline-block");
-								$("textarea[name='message']").css("display", "inline-block");
+								$("textarea[name='message']").css("display", "block");
 								$("textarea[name='message']").focus();
 			}
 		}
@@ -52,27 +51,57 @@ $(document).ready(function () {
 					$("input[type='submit']").show(1500);
 			}
 				let input = $(this).val().trim();
-				if (!infoLabel){
-						infoLabel = $(`<div> ${input.length} / 300 </div>`);
-						infoLabel.insertAfter($(this)); 
+				if (!counterLabel){
+						counterLabel = $(`<div> ${input.length} / 300 </div>`);
+						counterLabel.insertAfter($(this)); 
 				}
-				infoLabel.html(`<div> ${input.length} / 300</div>`);
+				counterLabel.html(`<div> ${input.length} / 300</div>`);
 						if (input.length >= 300){
-								infoLabel.css({
+								counterLabel.css({
 										"color": "#A50044",
 								});
 								$(this).css("border-bottom", "0.2rem solid #A50044");
-								setTimeout(() => {$(this).css("border-bottom", "0.2rem solid rgba(0, 64, 156, 0.5)"); infoLabel.css("color", "green");}, 2000);			
+								setTimeout(() => {$(this).css("border-bottom", "0.2rem solid rgba(0, 64, 156, 0.5)"); counterLabel.css("color", "green");}, 2000);			
 						}else{
-								infoLabel.css({
+								counterLabel.css({
 										"color": "green",
 								});
 						}
 				}
 		);
-		$(this).on("blur", function () {$(this).css("border-bottom", "0.2rem solid rgba(0, 64, 156, 0.5)");});
-		$(this).on('focus', function () {$(this).css("border-bottom", '0.2rem solid green');});
 		
+		$("textarea[name='message']").on("blur", function () {$(this).css("border-bottom", "0.2rem solid rgba(0, 64, 156, 0.5)");});
+		$("textarea[name='message']").on('focus', function () {$(this).css("border-bottom", '0.2rem solid green');});
+		
+		$("input[type='submit']").on("click", function (event) {
+				event.preventDefault();
+				if ($("textarea[name='message']").val().trim().length === 0){
+						if (!infoLabel){
+						infoLabel = $(`<div>Message field cant be empty.</div>`);
+						infoLabel.insertAfter($("textarea[name='message']")).css("color", "#A50044"); 
+				}
+				infoLabel.show();
+						setTimeout(() => {infoLabel.hide();}, 2000);
+				}else{
+						if(infoLabel){
+								infoLabel.hide();
+						}
+						$("#message").text("Email is being sent...\nPlease wait.");
+						$("#modalBox").fadeIn(1500);
+						setTimeout(() => {$("#modalBox").fadeOut(1500);}, 3000);
+						
+						emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, "#contactForm")
+						
+						.then(response => {	$("#message").text("Email sent.\nWe will get back to you as soon as possible");
+						$("#modalBox").fadeIn(1500);
+						setTimeout(() => {$("#modalBox").fadeOut(1500);}, 3000);})
+						
+						.catch(error =>{	$("#message").text("Email couldnct be sent.\nPlease try again later.");
+						$("#modalBox").fadeIn(1500);
+						setTimeout(() => {$("#modalBox").fadeOut(1500);}, 3000);});
+				}
+				
+		});
 		
 }
 		);
---
+++ scripts/secrets.js
@@ -0,0 +1,6 @@
+const PUBLIC_KEY = "hF3kg9QK6AUUcURu5";
+const SERVICE_ID = "service_ojfy4s4";
+const TEMPLATE_ID = "template_dfce1fu";
+
+export {PUBLIC_KEY, SERVICE_ID, TEMPLATE_ID};
+
