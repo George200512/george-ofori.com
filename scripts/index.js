1c5e41716ed503fc1c063ff79cc12dd423e908f7
Alpha <georgeofori2005@gmail.com>
2025-10-30 15:03:14 +0000

    Oct 30,2025

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,4 @@
 
 $(document).ready(function () {
-		$("body").fadeIn(1500);
-		$("footer").append(`<p style="text-align:center; padding-top:1vh">ATS &copy; ${new Date().getFullYear()}<p>`);
+		$(".footer-container").append(`<p style="text-align:center; padding-top:1vh">ATS &copy; ${new Date().getFullYear()}<p>`);
 });
-
-$("#businessButton").click(function () { 
-location.href = "business.html";
-}
-);
