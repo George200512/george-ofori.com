dede2eaa80eb854c2b933ceab9363b373bbce1cb
Alpha <georgeofori2005@gmail.com>
2025-11-07 15:37:02 +0000

    Nov 7, 2025

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
 
 $(document).ready(function () {
-		$(".footer-container").append(`<p style="text-align:center; padding-top:1vh">ATS &copy; ${new Date().getFullYear()}<p>`);
+		$(".footer-container").append(`<p style="text-align:center; padding-top:1vh; font-size:2vh;">ATS &copy; ${new Date().getFullYear()}<p>`);
 });
