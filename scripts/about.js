5c864153dbe37db4c749a7840a0c46b70809ea40
Alpha <georgeofori2005@gmail.com>
2025-12-03 12:40:19 +0000

    3 Dec , 2025

---
+++ scripts/about.js
@@ -0,0 +1,14 @@
+$(document).ready(
+function (event){
+		const dob = new Date(2005, 12, 25), currentDate = new Date();
+	let	age = currentDate.getFullYear() - dob.getFullYear();
+		if (!currentDate.getMonth() >= dob.getMonth() && currentDate.getDate() >=dob.getDate()){
+				age -= 1;
+		}
+		$("#age").text(`Age: ${age}`);
+		$(".section-2").slideDown(1000).css("display", "flex");
+}
+
+
+);
+
